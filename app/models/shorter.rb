53cb83d046667fabedf937ff9c9cd2c3daf0948e
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-09 16:54:13 +0300

    Additional methods and callback for Shorter model

--- app/models/shorter.rb
+++ app/models/shorter.rb
@@ -3,4 +3,21 @@ class Shorter < ApplicationRecord
   has_many :visitors, dependent: :destroy
   validates :original_url, presence: true
   validates :original_url, format: %r{\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z}ix
+  before_create :generate_data
+
+  def check_for_duplicates
+    Shorter.find_by(purified_url: purifying)
+  end
+
+  def purifying
+    original_url.downcase.gsub(%r{(https?:\/\/)|(http?:\/\/)|(www\.)}, '')
+  end
+
+  private
+
+    def generate_data
+      self.purified_url = purifying
+      self.code = SecureRandom.base58(6)
+      generate_data if Shorter.exists?(code: code)
+    end
 end
