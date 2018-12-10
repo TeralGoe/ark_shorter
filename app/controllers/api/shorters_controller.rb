8981f7384d77a360ab96114a1fce56cb3c844f8a
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-10 18:07:26 +0300

    Create method for API:Shorter controller

--- app/controllers/api/shorters_controller.rb
+++ app/controllers/api/shorters_controller.rb
@@ -5,5 +5,19 @@ module Api
       @shorters = Shorter.all
       json_response(@shorters)
     end
+
+    def create
+      duplicate = Shorter.new(shorter_params).check_for_duplicates
+      return json_response(duplicate, :created) if duplicate.present?
+
+      @shorter = Shorter.create!(shorter_params)
+      json_response(@shorter, :created)
+    end
+
+    private
+
+      def shorter_params
+        params.require(:shorter).permit(:original_url)
+      end
   end
 end
