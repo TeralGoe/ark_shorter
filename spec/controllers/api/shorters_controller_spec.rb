a0fe5ddbf8ebd569ac94c84a411234974933c0b8
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-10 18:52:34 +0300

    Test cases for API::Shorter showing

--- spec/controllers/api/shorters_controller_spec.rb
+++ spec/controllers/api/shorters_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::Shorters controller', type: :request do
   let!(:shorters) { create_list :shorter, 5 }
   let!(:visitors) { create_list :visitor, 5, shorter: shorters.first }
+  let(:shorter_code) { shorters.last.code }
 
   # List of shorters
   describe 'GET /api/shorters' do
@@ -48,6 +49,34 @@ RSpec.describe 'Api::Shorters controller', type: :request do
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  # Shorter showing
+  describe 'GET /api/shorters/:code' do
+    before { get "/api/shorters/#{shorter_code}" }
+
+    context 'when the record exists' do
+      it 'returns the shorter' do
+        expect(json).not_to be_empty
+        expect(json['code']).to eq(shorter_code)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:shorter_code) { '3.14' }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Shorter/)
       end
     end
   end
