e228c368722d1dd0640ee7aef2f033cc69e1239e
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-10 17:19:15 +0300

    Response concern for API modeule

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  # Cm
+  class ApplicationController < ActionController::API
+    include Response
+  end
+end
---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,6 @@
+# Cm
+module Response
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+end
