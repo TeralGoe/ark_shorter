b86878bfac3974cc9ee3db8235c4131343eff8f4
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-11 15:29:39 +0300

    Test case refactoring for Static controller

--- spec/controllers/web/static_controller_spec.rb
+++ spec/controllers/web/static_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Static controller', type: :request do
-  describe 'GET /' do
+RSpec.describe 'Web::Static controller', type: :request do
+  describe 'GET /docs' do
     context 'when layout work fine' do
-      before { get '/' }
+      before { get '/docs' }
       it 'show blank page' do
         expect(response.body).to include('a plan!')
       end
