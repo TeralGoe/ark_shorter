8f9d8146e496c087e03abd70e10aa47bb4d74c65
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2018-12-12 17:12:51 +0300

    Update application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope module: :web do
+    root to: 'shorters#new'
     get '/docs', to: 'static#docs'
+    resources :shorters, only: %i[create show edit], path: '', param: :code do
+      resources :visitors, only: %i[create]
+    end
   end
 
   namespace :api, defaults: { format: :json } do
